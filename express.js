b8b767bd4b7e4e20dc4950f20670b69cf56d805d
golyshevd <golyshev.dmitry@yandex.ru>
2014-03-26 04:03:48 +0400

    master: постарался сделать приложение на express более модульным

--- express.js
+++ express.js
@@ -5,92 +5,102 @@ var Express = require('express');
 var app = Express();
 var backend = require('./lib/backend');
 
-app.use(Express.cookieParser());
+var cookieParser = Express.cookieParser();
 
-// страница профиля
-app.get('/profile/', function (request, response) {
-
-    //  сначала надо авторизоваться
-    backend.auth(request.cookies.sessionid, function (err, res) {
-
-        //  ошибка авторизации
-        if ( err ) {
-            response.send(403);
-
-            return;
-        }
-
-        //  на странице надо отобразить
-        //  данные авторизации
-        //  статус пользователя
-        //  список его дел
-        var result = {
-            auth: res
-        };
-
-        var remaining = 2;
-
-        function done () {
-            remaining -= 1;
-
-            if ( 0 === remaining ) {
-                response.send(result);
-            }
-        }
-
-        //  загружаем статуст пользователя
-        backend.status(res.id, function (err, res) {
-            result.status = res;
-            done();
-        });
-
-        //  загружаем список дел пользователя
-        backend.deals(res.id, function (err, res) {
-            result.deals = res;
-            done();
-        });
+//  авторизация (не работает без cookieParser)
+function sessid (rq, rs, next) {
+    backend.sessid(rq.cookies.sessid, function (err, res) {
+        rq.sessid = res;
+        next();
     });
+}
 
+//  обязательная авторизация (не работает без sessid)
+function auth (rq, rs, next) {
+
+    if ( !rq.sessid ) {
+        rs.send(403);
+
+        return;
+    }
+
+    next();
+}
+
+//  status (не работает без sessid)
+function status (rq, rs, next) {
+    backend.status(rq.sessid.id, function (err, res) {
+        rq.status = res;
+        next();
+    });
+}
+
+//  сообщения пользователя (не работает без sessid)
+function messages (rq, res, next) {
+    backend.messages(rq.sessid.id, function (err, res) {
+        rq.messages = res;
+        next();
+    });
+}
+
+//  параллельно вызывает status и messages
+function statusAndMessages (rq, rs, next) {
+
+    var remaining = 2;
+
+    function done () {
+        remaining -= 1;
+
+        if ( 0 === remaining ) {
+            next();
+        }
+    }
+
+    status(rq, rs, done);
+    messages(rq, rs, done);
+}
+
+//  news (не работает без status)
+function news (rq, rs, next) {
+
+    if ( rq.status.canReadNews ) {
+        backend.news(function (err, res) {
+            rq.news = res;
+            next();
+        });
+
+        return;
+    }
+
+    rs.send(403);
+}
+
+//  robots.txt
+app.get('/robots.txt', function (rq, rs) {
+    rs.type('text');
+    rs.send([
+        'Host: ' + rq.host,
+        'User-agent: *'
+    ].join('\n'));
 });
 
 //  страница новостей
-app.get('/news/', function (request, response) {
-
-    //  требуется авторизация
-    backend.auth(request.cookies.sessionid, function (err, res) {
-
-        //  ошибка авториазации
-        if ( err ) {
-            response.send(403);
-
-            return;
-        }
-
-        //  надо отобразить
-        //  данные авторизации
-        //  ленту новостей
-        var result = {
-            auth: res
-        };
-
-        //  загружаем статус пользователя чтобы узнать
-        //  может ли он читать новости
-        backend.status(res.id, function (err, res) {
-            /*eslint max-nested-callbacks: [2, 4]*/
-            if ( res.canReadPortalNews ) {
-                //  загружаем ленту новостей
-                backend.news(function (err, res) {
-                    result.news = res;
-                    response.send(result);
-                });
-
-                return;
-            }
-
-            //  не может
-            response.send(403);
-        });
+app.get('/news/', cookieParser, sessid, auth, status, news, function (rq, rs) {
+    rs.send({
+        sessid: rq.sessid,
+        news: rq.news
     });
 });
+
+// страница профиля
+app.get('/profile/', cookieParser, sessid, auth,
+//    status, messages,     //  ТАК НЕ ОПТИМАЛЬНО, лучше параллельно
+    statusAndMessages, function (rq, rs) {
+        rs.send({
+            sessid: rq.sessid,
+            status: rq.status,
+            messages: rq.messages
+        });
+    });
 
 app.listen(1338);
--- fist.js
+++ fist.js
@@ -6,34 +6,40 @@ var app = new Fist();
 var backend = require('./lib/backend');
 
 //  Узел авторизации
-//  При любой ошибке бэкенда отправляем 403
-app.decl('auth', function (track, errors, result, done) {
-    backend.auth(track.cookie('sessionid'), function (err, res) {
+app.decl('sessid', function (track, errors, result, done) {
+    backend.sessid(track.cookie('sessid'), done);
+});
 
-        if ( err ) {
-            track.send(403);
+//  Обязательная авторизация
+app.decl('auth', ['sessid'], function (track, errors) {
 
-            return;
-        }
+    if ( errors.sessid ) {
+        track.send(403);
 
-        done.apply(this, arguments);
-    });
+        return false;
+    }
+
+    return true;
 });
 
 //  Узел статуса пользователя
-app.decl('status', ['auth'], function (track, errors, result, done) {
-    backend.status(result.auth.id, done);
+app.decl('status', [
+    'auth', 'sessid'
+], function (track, errors, result, done) {
+    backend.status(result.sessid.id, done);
 });
 
-//  Усзел списка дел пользователя
-app.decl('deals', ['auth'], function (track, errors, result, done) {
-    backend.deals(result.auth.id, done);
+//  Узел списка сообщений пользователя
+app.decl('messages', [
+    'auth', 'sessid'
+], function (track, errors, result, done) {
+    backend.messages(result.sessid.id, done);
 });
 
 //  Узел ленты новостей, новости может видеть не каждый
 app.decl('news', ['status'], function (track, errors, result, done) {
 
-    if ( result.status.canReadPortalNews ) {
+    if ( result.status.canReadNews ) {
         backend.news(done);
 
         return;
@@ -43,19 +49,30 @@ app.decl('news', ['status'], function (track, errors, result, done) {
 });
 
 //  Отображает страницу профиля
-app.decl('userProfilePage', [
-    'status', 'deals', 'auth'],
-    function (track, errors, result) {
+app.decl('profilePage', [
+    'status', 'messages', 'sessid'
+], function (track, errors, result) {
         track.send(result);
-    });
+});
 
 //  Отображает страницу новостей
-app.decl('newsPage', ['news', 'auth'], function (track, errors, result) {
+app.decl('newsPage', [
+    'news', 'sessid'
+], function (track, errors, result) {
     track.send(result);
 });
 
+//  robots.txt
+app.decl('robots', function (track) {
+    track.send([
+        'Host: ' + track.url.hostname,
+        'User-agent: *'
+    ].join('\n'));
+});
+
 //  настройка роутера
-app.route('GET', '/profile/', 'userProfilePage');
+app.route('GET', '/robots.txt', 'robots');
 app.route('GET', '/news/', 'newsPage');
+app.route('GET', '/profile/', 'userProfilePage');
 
 app.listen(1337);
