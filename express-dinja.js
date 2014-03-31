7be5b3a774a3bad0e8a0ec1e8f54df8bf71890ff
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-04-01 02:09:56 +0600

    Using express-dinjga

---
+++ express-dinja.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+var backend = require('./lib/backend');
+
+var cookies = express.cookieParser();
+
+var inject = require('express-dinja')(app);
+
+inject('cookies', cookies);
+
+inject('sessid', function sessid(cookies, req, res, next) {
+    backend.sessid(req.cookies.sessid, next);
+});
+
+inject('status', function status(sessid, req, res, next) {
+    backend.status(sessid.id, next);
+});
+
+inject('todos', function todos(sessid, req, res, next) {
+    backend.todo(sessid.id, next);
+});
+
+app.get('/todo/', function (sessid, status, todos, req, res) {
+    res.send({
+        sessid: req.sessid,
+        status: req.status,
+        todos: req.todos
+    });
+});
+
+inject('users', function users(req, res, next) {
+    backend.users(next);
+});
+
+inject('ads', function ads(req, res, next) {
+    backend.ads(next);
+});
+
+app.get('/robots.txt', function (req, res) {
+    res.send([
+        'Host: ' + req.host,
+        'User-agent: *'
+    ].join('\n'));
+});
+
+app.get('/', function (ads, users, sessid, req, res) {
+    res.send({
+        sessid: sessid,
+        users: users,
+        ads: ads
+    });
+});
+
+app.listen(1338);
